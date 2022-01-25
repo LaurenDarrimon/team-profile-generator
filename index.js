54cee926d61ba6f84fe58ae21a7adb730ca08bde
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-24 17:38:46 -0800

    add consitionals to inquirer prompts that depend on the role that the user selected

--- index.js
+++ index.js
@@ -7,29 +7,41 @@ const inquirer = require("inquirer");
     // Create an array of questions for user input
 const questions = 
 [  
-  
     {
         type: 'list',
         message: 'What type of team member would you like to add?',
         name: 'role',
         choices: [ "Manager", "Engineer", "Intern", "Done"],
     },
+];
+
+const managerQuestions = 
+[
     {
         type: 'input',
         message: 'Enter manager office number:',
         name: 'office',
     },
+];
+
+const engineerQuestions = 
+[
     {
         type: 'input',
         message: 'Enter engineer github username:',
         name: 'gitUser',
     },
+];
+
+const internQuestions = 
+[
     {
         type: 'input',
         message: 'Enter the school the intern attends:',
         name: 'school',
     },
 ];
+
 
 
 
@@ -45,7 +57,34 @@ function init() {
     .then((response) => {  //only after we get prompts, then 
         console.log(response);
   
-         //object destructuring to pull out role name 
+        const { role } = response; //object destructuring to pull out role name 
+
+        //write conditionals that determine which set of follow up questions to ask 
+        if (role === "Manager"){
+            inquirer
+                .prompt(managerQuestions)
+                .then ((managerResponse) => {
+                    console.log(managerResponse);
+                });
+        }
+        if (role === "Engineer"){
+            inquirer
+                .prompt(engineerQuestions)
+                .then ((engineerResponse) => {
+                    console.log(engineerResponse);
+                });
+        }
+        if (role === "Intern"){
+            inquirer
+                .prompt(internQuestions)
+                .then ((internResponse) => {
+                    console.log(internResponse);
+                });
+        }
+        if (role === "Done"){
+            console.log("the code is done, run write file fxn");
+               
+        }
 
         //call fxn to write the file with paramenters of the filename 
         //eventually we'll pass the returned HTML data from on other js files 
