5984ae19737d584c767a23a3c128a16ced5e62d6
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-25 20:07:37 -0800

    run inquirer prompt from classes belonging to Employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -5,6 +5,55 @@ const Intern  = require("../lib/Intern");
 
 //The first class is an `Employee` parent class with the following properties and methods:
 
+
+describe("Employee class", () => {
+    
+    //ATTRIBUTES - tests for attributes belonging to Employee
+    it(`name`, () => {
+
+      expect(new Employee("?").visible).toBe(true); 
+    });
+    
+    it(`id`, () => {
+      expect(new Employee("a").visible).toBe(false); 
+    });
+  
+    it(`email`, () => {
+      expect(new Employee("a").toString()).toBe("_");
+    });
+  
+    describe (`getName()`, () => {
+      it("gets the name")  
+      expect(new Employee("?").toString()).toBe("?");
+    });
+
+
+    //METHODS - tests for methods belonging to Employee
+    describe(`getId()`, () => {
+      it("gets the ID", () => {
+        expect(new Employee().method(arg)).toBe(something);
+      });
+    });
+
+
+    describe(`getEmail()`, () => {
+      it("returns character", () => {
+        expect(new Employee("l").getSolution()).toBe("l");
+      });
+    });
+  
+    describe(`getRole()`, () => {
+    
+      it("returns Employee", () => {
+        expect(new Letter("l").getSolution()).toBe("l");
+      });
+    });
+  });
+
+
+
+
+
 `name`
 
 `id`
@@ -19,5 +68,4 @@ const Intern  = require("../lib/Intern");
 
 `getRole()`//returns `'Employee'`
 
-//The other three classes will extend `Employee`.
 
--- index.js
+++ index.js
@@ -2,52 +2,17 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-//require js files in src directory - template helper codes 
-const Employee = require("src/Employee");
+//require js files in lib directory 
+const Employee = require("./lib/Employee");  
+//const Engineer  = require("./lib/Engineer"); 
+//const Manager  = require("./lib/Manager");
+//const Intern  = require("./lib/Intern");
+
+//require template helper codes files 
 
 
+const employee = new Employee();
 
-    // Create an array of questions for user input
-const questions = 
-[  
-    {
-        type: 'list',
-        message: 'What type of team member would you like to add?',
-        name: 'role',
-        choices: [ "Manager", "Engineer", "Intern", "Done"],
-    },
-];
-
-const managerQuestions = 
-[
-    {
-        type: 'input',
-        message: 'Enter manager office number:',
-        name: 'office',
-    },
-];
-
-const engineerQuestions = 
-[
-    {
-        type: 'input',
-        message: 'Enter engineer github username:',
-        name: 'gitUser',
-    },
-];
-
-const internQuestions = 
-[
-    {
-        type: 'input',
-        message: 'Enter the school the intern attends:',
-        name: 'school',
-    },
-];
-
-
-
-//writeToFile(response); 
 
 
 // Create a function to initialize app
@@ -56,52 +21,17 @@ function init() {
     inquirer
      //prompt user for questions 
 
-    .prompt(questions) 
+    .prompt(employee.questions) 
 
     .then((response) => {  //only after we get prompts, then 
         console.log(response);
   
-        const { role } = response; //object destructuring to pull out role name 
-
-        //write conditionals that determine which set of follow up questions to ask 
-        if (role === "Manager"){
-            inquirer
-                .prompt(managerQuestions)  //ask manager questions
-                .then ((managerResponse) => {
-                    console.log(managerResponse);
-                })
-                .then (() => {
-                    init();  //ask again if there is another team member to add. 
-                });
-        }
-        if (role === "Engineer"){
-            inquirer
-                .prompt(engineerQuestions) //ask engineer questions
-                .then ((engineerResponse) => {
-                    console.log(engineerResponse);
-                })
-                .then (() => {
-                    init(); //ask again if there is another team member to add. 
-                });
-        }
-        if (role === "Intern"){
-            inquirer
-                .prompt(internQuestions) //ask intern questions
-                .then ((internResponse) => {
-                    console.log(internResponse);
-                })
-                .then (() => {
-                    init(); ///ask again if there is another team member to add. 
-                });
-        }
-        if (role === "Done"){
-            console.log("the code is done, run write file fxn");
-                //call fxn to write the file with paramenters of the filename 
-                //eventually we'll pass the returned HTML data from on other js files  
-        }
-       
+        //const { role } = response; //object destructuring to pull out role name 
         
     });
 }
 
+
 init();  // Function call to initialize app
+
+//writeToFile(response); 
